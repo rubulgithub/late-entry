68d76b63a2c4b1386f4e054e6fdea0eabcd02c0c
rubulgithub <rubulislam005@gmail.com>
2022-10-06 13:03:50 +0530

    we are trying to push in git

--- MyJavaCodes/src/Test.java
+++ MyJavaCodes/src/Test.java
@@ -1,4 +1,5 @@
 public class Test {
-    System.out.println("Hello world this is java test");
-    
+    public static void main(String[] args) {
+        System.out.println("Hello world this is java test");
+    }
 }
