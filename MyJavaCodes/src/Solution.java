2ef8ee59b5ec114a22e07fcf2899df7eeebc9e3c
rubulgithub <rubulislam005@gmail.com>
2022-10-08 15:35:12 +0530

    ewfre

---
+++ MyJavaCodes/src/Solution.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Solution 
+{
+
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter how many terms: ");
+		int t = sc.nextInt();
+		for (int i = 0; i < t; i++) 
+        {
+            System.out.println("Enter a: ");
+			int a = sc.nextInt();
+            System.out.println("Enter b: ");
+			int b = sc.nextInt();
+            System.out.println("Enter n: ");
+			int n = sc.nextInt();
+
+			int sum = a;
+			for (int j = 0; j < n; j++)  //j=0 ,  a= 3, b= 2, n=3
+            {
+				sum += b * Math.pow(2, j); //sum= 3+2*2^0=5
+				System.out.println("Sum of the Series : " +sum);
+			}
+			System.out.println();
+		}
+    }
+}
